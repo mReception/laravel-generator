3db55ae094a0e72a07e2d5f99e384ad6195c6d11
My Rent <sergey.sap@my-rent.net>
2023-03-04 10:44:36 +0100

    new features:
    ResponseUtil

--- src/Utils/ResponseUtil.php
+++ src/Utils/ResponseUtil.php
@@ -2,10 +2,22 @@
 
 namespace InfyOm\Generator\Utils;
 
+use Illuminate\Pagination\AbstractCursorPaginator;
+use Illuminate\Pagination\AbstractPaginator;
+
 class ResponseUtil
 {
     public static function makeResponse(string $message, mixed $data): array
     {
+
+        if (!is_array($data) && !is_string($data) && ($data->resource instanceof AbstractPaginator || $data->resource instanceof AbstractCursorPaginator)) {
+            return array_merge(
+                $data->resource->toArray(),[
+                'success' => true,
+                'message' => $message,
+            ]);
+        }
+
         return [
             'success' => true,
             'data'    => $data,
