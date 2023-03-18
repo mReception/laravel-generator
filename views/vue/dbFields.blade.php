fbef6fef6f7ea90683b3bc7cf6e1ee12f8d5b17e
leonn366 <leonn366@gmail.com>
2023-03-18 22:12:05 +0200

    fix

--- views/vue/dbFields.blade.php
+++ views/vue/dbFields.blade.php
@@ -6,6 +6,6 @@ export const dbFields = [
 
 export const dbFieldsTypes = {
 @foreach($properties as $name => $property)
-    '{{ $property['field_name'] }}': @if(substr($property['field_name'], -3) === '_id') 'select' @else 'input'@endif,
+    '{{ $property['field_name'] }}': @if(substr($property['field_name'], -3) === '_id') 'select' @elseif(strpos($property['field_name'], 'is_' ) !== false) 'checkbox'@else 'input'@endif,
 @endforeach
 }
