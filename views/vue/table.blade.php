03874971f9aaa652be6f393e8fbb7352b311af62
leonn366 <leonn366@gmail.com>
2023-03-18 11:10:50 +0200

    fix

--- views/vue/table.blade.php
+++ views/vue/table.blade.php
@@ -2,25 +2,20 @@
     <div>
         <base-table
                 title="{{ $config->modelNames->camelPlural }}"
-                :fields="fields"
+                :fields="columns"
                 :store="store"
         />
     </div>
-
 </template>
 
-
 <script setup>
+import BaseTable from 'components/layouts/BaseTable.vue';
+import {reactive} from "vue";
+import {dbFields} from "src/use/dbConsts/{{ $config->modelNames->dashed }}";
+import {columnsFromDbFields} from "src/use/baseTableHelper";
+import {use{{ $config->modelNames->camelPlural }}} from "stores/{{ $config->modelNames->dashed }}";
 
-    import BaseTable from 'components/layouts/BaseTable.vue';
-    import {reactive} from "vue";
-    import {dbFields} from "src/use/dbConsts/{{ $config->modelNames->dashed }}";
-    import {columnsFromDbFields} from "src/use/baseTableHelper";
-    import {useProcessingPriorities} from "stores/{{ $config->modelNames->dashed }}";
-
-    const store = use{{ $config->modelNames->camelPlural }}()
-    const fields = [];
-    const columns = reactive(columnsFromDbFields(dbFields));
-
+const store = use{{ $config->modelNames->camelPlural }}()
+const columns = reactive(columnsFromDbFields(dbFields));
 
 </script>
