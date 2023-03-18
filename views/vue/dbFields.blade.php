ba3c1d8bc26874ae8af4bf0f4e7198aaa7232ea0
leonn366 <leonn366@gmail.com>
2023-03-18 22:01:19 +0200

    fix

--- views/vue/dbFields.blade.php
+++ views/vue/dbFields.blade.php
@@ -1,6 +1,11 @@
 export const dbFields = [
-    @foreach($properties as $name => $property)
-        '{{ $property['js_name'] }}',
-    @endforeach
+@foreach($properties as $name => $property)
+    '{{ $property['field_name'] }}',
+@endforeach
 ];
 
+export const dbFieldsTypes = {
+@foreach($properties as $name => $property)
+    '{{ $property['field_name'] }}': @if(substr($property['field_name'], -3) === '_id') 'select' @else 'input'@endif,
+@endforeach
+}
